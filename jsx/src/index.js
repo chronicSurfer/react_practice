457c4bc0ead20b1afc07f611388496241da4160e
chronicSurfer <kyle.umbarger@gmail.com>
2019-01-02 16:40:17 -0800

    Changed button text to show how to reference object in React

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,19 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText () {
-    return 'Click on me!';
-}
 
 // create a react component
 const App = () => {
+
+    const buttonText = {text: "Click Me!"};
 
     return (
         <div>
             <label className="label" for="name">Enter your name: </label>
             <input id="name" type="text"/>
             <button style={{backgroundColor: 'blue', color: 'white'}}>
-            {getButtonText()}
+            {buttonText.text}
             </button>
         </div>
     );

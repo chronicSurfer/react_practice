a7f6d07e4f836ff6e49253d1e1365cdfbdc83436
chronicSurfer <kyle.umbarger@gmail.com>
2019-01-02 16:32:41 -0800

    Functional component with function reference for button

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,15 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText () {
+    return 'Click on me!';
+}
+
 // create a react component
 const App = () => {
+
     return (
-        <div>Hi there!</div>
+        <div>
+            <label className="label" for="name">Enter your name: </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {getButtonText()}
+            </button>
+        </div>
     );
 };
 
 //Show react component on the screen
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+
+

85cef1e8b7b2e6f76f1a82ae9c14d1c39ae0fe57
chronicSurfer <kyle.umbarger@gmail.com>
2019-01-08 00:31:23 -0800

    Passed State As Props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
@@ -34,7 +34,7 @@ class App extends React.Component {
                 if(this.state.errorMessage && !this.state.lat){
                     return <div>Error: {this.state.errorMessage}</div>
                 } else if (this.state.lat && !this.state.errorMessage) {
-                    return <div>Latitude: {this.state.lat}</div>
+                    return <div><SeasonDisplay lat={this.state.lat}/></div>
                 } else {
                     return <div>Loading!</div>
                 }
